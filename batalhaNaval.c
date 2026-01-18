57f764fcb917a23f83abb89a8f4b875d620845ce
jigsawsd <jigsawsec@gmail.com>
2026-01-18 07:46:22 +0000

    Nível Aventureiro OK.

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,34 +1,49 @@
 #include <stdio.h>
 
+#define TAM 10
+#define AGUA 0
+#define NAVIO 3
+
 int main() {
-    int tabuleiro[10][10];
+    int tabuleiro[TAM][TAM];
     int i, j;
     
-    // Inicializa tabuleiro com zeros
-    for(i = 0; i < 10; i++) {
-        for(j = 0; j < 10; j++) {
-            tabuleiro[i][j] = 0;
+    // Inicializa tudo com agua
+    for(i = 0; i < TAM; i++) {
+        for(j = 0; j < TAM; j++) {
+            tabuleiro[i][j] = AGUA;
         }
     }
     
-    // Navio 1 - Horizontal (linha 2, começa na coluna 3)
-    tabuleiro[2][3] = 3;
-    tabuleiro[2][4] = 3;
-    tabuleiro[2][5] = 3;
+    // Navio 1 - horizontal na linha 2
+    tabuleiro[2][1] = NAVIO;
+    tabuleiro[2][2] = NAVIO;
+    tabuleiro[2][3] = NAVIO;
     
-    // Navio 2 - Vertical (coluna 7, começa na linha 5)
-    tabuleiro[5][7] = 3;
-    tabuleiro[6][7] = 3;
-    tabuleiro[7][7] = 3;
+    // Navio 2 - vertical na coluna 7
+    tabuleiro[5][7] = NAVIO;
+    tabuleiro[6][7] = NAVIO;
+    tabuleiro[7][7] = NAVIO;
+    
+    // Navio 3 - diagonal descendo (linha 0, coluna 5)
+    tabuleiro[0][5] = NAVIO;
+    tabuleiro[1][6] = NAVIO;
+    tabuleiro[2][7] = NAVIO;
+    
+    // Navio 4 - diagonal subindo (linha 9, coluna 0)
+    tabuleiro[9][0] = NAVIO;
+    tabuleiro[8][1] = NAVIO;
+    tabuleiro[7][2] = NAVIO;
     
     // Exibe o tabuleiro
-    printf("Tabuleiro:\n");
-    for(i = 0; i < 10; i++) {
-        for(j = 0; j < 10; j++) {
+    printf("\nTabuleiro de Batalha Naval:\n\n");
+    for(i = 0; i < TAM; i++) {
+        for(j = 0; j < TAM; j++) {
             printf("%d ", tabuleiro[i][j]);
         }
         printf("\n");
     }
+    printf("\n");
     
     return 0;
 }
